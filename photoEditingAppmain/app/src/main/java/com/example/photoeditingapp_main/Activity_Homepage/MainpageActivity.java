c09a2df9214d162e296881e4f7226af2b3ee673f
Khanh Nguyen <khanhnguyen02311@gmail.com>
2022-04-23 14:36:58 +0700

    Minor changes

---
+++ photoEditingAppmain/app/src/main/java/com/example/photoeditingapp_main/Activity_Homepage/MainpageActivity.java
@@ -0,0 +1,16 @@
+package com.example.photoeditingapp_main.Activity_Homepage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.photoeditingapp_main.R;
+
+public class MainpageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mainpage);
+    }
+}
